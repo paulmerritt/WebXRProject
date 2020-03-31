d53c261e02a68c2af527dffd996350528690f03d
Paul Merritt <pmerritt@pscr.lz>
2020-03-31 12:04:10 -0600

    sample a-scene with auto-refreshing the page to receive new IoT data

--- tcpexress.js
+++ tcpexress.js
@@ -3,6 +3,8 @@ const session = require('express-session');
 const app = express();
 var compression = require('compression')
 var net = require('net');
+var reload = require('reload');
+
 
 var client = new net.Socket();
 var client1 = new net.Socket();
@@ -111,22 +113,28 @@ client3.on('close', function() {
     console.log('Connection closed');
 });
 
+// var myChangingNumber = 0
+
+// setInterval(function() {
+//   myChangingNumber++
+// }, 1000);
 
 
 
-app.get( '/', function( req, res ) {
+app.get('/',function( req, res ) {
     
-    function updateFoo(){
+    //function updateFoo(){
     
         var outp = recentOut + "\n" + recentOut1 + "\n" + recentOut2 + "\n" + recentOut3;
 
-        //res.render('index', {layout: false, out: outp});
-
-        res.write('<p>'+outp+'</p>');           
-        setTimeout( updateFoo, 3000); 
-    }
-    updateFoo();
+        res.render('index', {layout: false, out: outp});
+        //res.write('<p>' + outp + '</p>');
+        //res.send('<a-scene><a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box><a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere><a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder><a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>        <a-sky color="#ECECEC"></a-sky></a-scene><h1>Output from 4 servers:</h1><p id="demo">'+outp+'</p>');           
+        //setTimeout( updateFoo, 3000); 
+    //}
+    //updateFoo();
 });
+
 
 
 app.listen(8080);
