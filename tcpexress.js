f3cd743a7758093fb33682bd1ff449134fb794a7
Paul Merritt <pmerritt@pscr.lz>
2020-05-15 17:09:31 -0600

    got it working with dynamic amount and variety of sources via sources.json

--- tcpexress.js
+++ tcpexress.js
@@ -6,179 +6,88 @@ var net = require('net');
 var reload = require('reload');
 var fs = require('fs');
 var https = require('https');
+var exphbs = require('express-handlebars');
 
-var client = new net.Socket();
-var client1 = new net.Socket();
-var client2 = new net.Socket();
-var client3 = new net.Socket();
+var sources = [];
 
-var recentOut = "";
-var recentOut1 = "";
-var recentOut2 = "";
-var recentOut3 = "";
-
-var output = "";
-
-var array = [];
-
-var coords = {
-    lat:0.0, 
-    long:0.0, 
-};
-
-var coords1 = {
-    lat:0.0, 
-    long:0.0, 
-};
-
-var coords2 = {
-    lat:0.0, 
-    long:0.0, 
-};
-
-var coords3 = {
-    lat:0.0, 
-    long:0.0, 
-};
-
-function containsObject(obj, list) {
-    var i;
-    for (i = 0; i < list.length; i++) {
-        if (list[i] === obj) {
-            return true;
-        }
-    }
-
-    return false;
+var source_obj = {
+    ip_address: "",
+    port: 0,
+    client: null,
+    out: "",
+    coords: 
+        {
+            latitude: 0.0, longitude: 0.0
+        },
+    marker: ""
 }
 
-app.set('view engine', 'hbs');
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 
-app.use(compression());
+var data = null;
 
-client.connect(57000, 'localhost', function() {
-    console.log('Connected');
-    //client.write('Hello, server! Love, Client.');
-});
+const fileContents = fs.readFileSync('./source.json', 'utf8')
 
-client.on('data', function(data) {
-    console.log(data.toString());
-    //array.push(data.toString());
-    var j = JSON.parse(data.toString());
+try {
+  data = JSON.parse(fileContents)
+  //console.log(data.sources);
+} catch(err) {
+  console.error(err)
+}
 
-    coords.lat = j.coords.latitude;
-    coords.long = j.coords.longitude;
+
+data.sources.forEach(src=>{
+
+    var source = Object.create(source_obj);
+    source.ip_address = src.server_ip;
+    source.port = src.port;
+    source.client = new net.Socket();
+
+
+    source.client.connect(source.port, source.ip_address, function() {
+        //console.log('Connected');
+    });
     
-    recentOut = data.toString();
+    source.client.on('data', function(data) {
+        var j = JSON.parse(data.toString());
+    
+        source.coords.latitude = j.coords.latitude;
+        source.coords.longitude = j.coords.longitude;
+        source.marker = j.marker;
         
+        source.out = JSON.stringify(j);
+        //console.log(source.out);
+    });
+    
+    source.client.on('close', function() {
+        console.log('Connection closed');
+    });
+
+    sources.push(source);
+
 });
-
-client.on('close', function() {
-    console.log('Connection closed');
-});
-//-----------------------------------------------------
-client1.on('close', function() {
-    console.log('Connection closed');
-});
-
-client1.connect(57001, 'localhost', function() {
-    console.log('Connected');
-    //client.write('Hello, server! Love, Client.');
-});
-
-client1.on('data', function(data) {
-    console.log(data.toString());
-    //array.push(data.toString());
-
-    var j = JSON.parse(data.toString());
-
-    coords1.lat = j.coords.latitude;
-    coords1.long = j.coords.longitude;
-
-    recentOut1 = data.toString();
-        
-});
-
-client1.on('close', function() {
-    console.log('Connection closed');
-});
-
-//-----------------------------------------------------
-client2.on('close', function() {
-    console.log('Connection closed');
-});
-
-client2.connect(57002, 'localhost', function() {
-    console.log('Connected');
-    //client.write('Hello, server! Love, Client.');
-});
-
-client2.on('data', function(data) {
-    console.log(data.toString());
-    //array.push(data.toString());
-
-    var j = JSON.parse(data.toString());
-
-    coords2.lat = j.coords.latitude;
-    coords2.long = j.coords.longitude;
-
-    recentOut2 = data.toString();
-        
-});
-
-client2.on('close', function() {
-    console.log('Connection closed');
-});
-
-//-----------------------------------------------------
-client3.on('close', function() {
-    console.log('Connection closed');
-});
-
-client3.connect(57003, 'localhost', function() {
-    console.log('Connected');
-    //client.write('Hello, server! Love, Client.');
-});
-
-client3.on('data', function(data) {
-    console.log(data.toString());
-    //array.push(data.toString());
-
-    var j = JSON.parse(data.toString());
-
-    coords3.lat = j.coords.latitude;
-    coords3.long = j.coords.longitude;
-
-    recentOut3 = data.toString();
-        
-});
-
-client3.on('close', function() {
-    console.log('Connection closed');
-});
-
-// var myChangingNumber = 0
-
-// setInterval(function() {
-//   myChangingNumber++
-// }, 1000);
-
-
+    
+app.get('/sources', (req, res) => {
+    
+    res.send(sources);
+  });
 
 app.get('/',function( req, res ) {
     
-    //function updateFoo(){
-    
-        var outp = "[" + recentOut + ",\n" + recentOut1 + ",\n" + recentOut2 + ",\n" + recentOut3 + "]";
+        const q = {};
+        var outp = "[";//"[" + recentOut + ",\n" + recentOut1 + ",\n" + recentOut2 + ",\n" + recentOut3 + "]";
 
-        var gpsp = "latitude: " + coords.lat + "; longitude: " + coords.long + ";";
+        sources.forEach(source=>{
+            outp += source.out + ",\n";
+        });
 
-        res.render('index', {layout: false, out: outp, gps: gpsp});
-        //res.write('<p>' + outp + '</p>');
-        //res.send('<a-scene><a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box><a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere><a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="#FFC65D"></a-cylinder><a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>        <a-sky color="#ECECEC"></a-sky></a-scene><h1>Output from 4 servers:</h1><p id="demo">'+outp+'</p>');           
-        //setTimeout( updateFoo, 3000); 
-    //}
-    //updateFoo();
+        outp += "]";
+
+        //var gpsp = "latitude: " + coords.lat + "; longitude: " + coords.long + ";";
+
+        res.render('index', {layout: false, sources_array: sources});
+
 });
 
 
