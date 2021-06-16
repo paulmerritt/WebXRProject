b02e5fefbb912f45902397b288b35dea3b05d2a7
Paul Merritt <pmerritt@pscr.lz>
2021-06-16 13:47:43 -0600

    adding test potree in aframe

--- tcpexress.js
+++ tcpexress.js
@@ -7,6 +7,7 @@ var reload = require('reload');
 var fs = require('fs');
 var https = require('https');
 var exphbs = require('express-handlebars');
+var cors = require('cors');
 
 var sources = [];
 
@@ -20,10 +21,12 @@ var source_obj = {
             latitude: 0.0, longitude: 0.0
         },
     marker: "",
-    color: ""
+    color: "",
+    bottom_color: ""
 }
-
+app.use(cors());
 app.engine('handlebars', exphbs());
+app.use("/dist", express.static('./dist/'));
 app.set('view engine', 'handlebars');
 
 var data = null;
@@ -37,6 +40,14 @@ try {
   console.error(err)
 }
 
+// function hexToRgb(hex) {
+//     var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+//     return result ? {
+//       r: parseInt(result[1], 16),
+//       g: parseInt(result[2], 16),
+//       b: parseInt(result[3], 16)
+//     } : null;
+//   }
 
 data.sources.forEach(src=>{
 
@@ -44,8 +55,8 @@ data.sources.forEach(src=>{
     source.ip_address = src.server_ip;
     source.port = src.port;
     source.client = new net.Socket();
-    source.color = "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
-        
+    source.color = "" + Math.floor(Math.random() * 256).toString(); + " " + Math.floor(Math.random() * 256).toString(); + " " + Math.floor(Math.random() * 256).toString();//"#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
+    source.bottom_color = "" + Math.floor(Math.random() * 256).toString(); + " " + Math.floor(Math.random() * 256).toString(); + " " + Math.floor(Math.random() * 256).toString();//"#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
 
     source.client.connect(source.port, source.ip_address, function() {
         //console.log('Connected');
@@ -80,10 +91,20 @@ app.get('/sources', (req, res) => {
 
 app.get('/',function( req, res ) {
     
-    res.render('index', {layout: false, sources_array: sources});
+    res.render('prog', {layout: false, sources_array: sources});
 
 });
 
+app.get('/testlas',function( req, res ) {
+
+    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+    // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // res.setHeader('Access-Control-Allow-Credentials', true);
+    
+    res.render('testlas', {layout: false});
+
+});
 
 var privateKey  = fs.readFileSync('server.key', 'utf8');
 var certificate = fs.readFileSync('server.cert', 'utf8');
